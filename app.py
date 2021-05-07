55ac6cb4d1e8bd05ac37b233434d5e18aeae6313
Taru-garg <tarugarg2@gmail.com>
2021-05-07 16:24:23 +0530

    Flask Port and Jquery

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+@app.route('/search', methods=['POST'])
+def searchInput():
+    data = request.get_json()
+    print(data)
+    return ''
+
+@app.route('/search/go', methods=['POST'])
+def searchEnter():
+    data = request.get_json()
+    print(data)
+    return ''
 
 @app.route('/')
 def home():
--- static/assets/scripts/search.js
+++ static/assets/scripts/search.js
@@ -1,4 +1,27 @@
-function extractData(searchString) {
-    splitString = searchString.split(';');
-    console.log(splitString);
+// Function to send the data to the server
+function sendData(searchString, urlPassed) {
+    $.ajax({
+        type: 'POST',
+        url: urlPassed,
+        data: JSON.stringify({ 'data': searchString }),
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+    });
 }
+
+// Wait for the document to load
+$(document).ready(function () {
+    $('#nav__search').on('input', function () {
+        // Send any data input in the input search box
+        sendData($(this).val(), '/search');
+    });
+    // Check for keypress
+    $('#nav__search').on('keypress', function (event) {
+        var code = event.keyCode || event.which;
+        // Check the keycode
+        if (code == 13) {
+            // Perform Action (send Data)
+            sendData($(this).val(), '/search/go');
+        }
+    });
+});
