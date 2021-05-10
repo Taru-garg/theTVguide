9e2e177d47e33515e85736570e87e4c5b9540478
Taru-garg <tarugarg2@gmail.com>
2021-05-10 13:56:59 +0530

    Implementation of Cleaning and Seprating the Search String

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect
+from utility import searchCleaning
 app = Flask(__name__)
 
 @app.route('/search', methods=['POST'])
 def searchInput():
     data = request.get_json()
-    print(data)
-    return ''
+    # print(data)
+    response = jsonify(
+        data = searchCleaning.sanitize(data['data']),
+        status=200,
+        mimetype = 'application/json'
+    )
+    return response
 
 @app.route('/search/go', methods=['POST'])
 def searchEnter():
     data = request.get_json()
-    print(data)
-    return ''
+    # print(data)
+    return redirect('/')
 
 @app.route('/')
 def home():
--- static/assets/scripts/search.js
+++ static/assets/scripts/search.js
@@ -6,6 +6,8 @@ function sendData(searchString, urlPassed) {
         data: JSON.stringify({ 'data': searchString }),
         contentType: "application/json;charset=utf-8",
         dataType: "json",
+    }).done(function(response) {
+        console.log(JSON.stringify(response));
     });
 }
 
@@ -24,4 +26,38 @@ $(document).ready(function () {
             sendData($(this).val(), '/search/go');
         }
     });
+    // autocomplete part search
+    // Posting anything to the server
+    // be done here and obtained in search var
+    $(function () {
+        // this is to brought from the server
+        // further it will then we used, a json
+        // returned must have atleast 2 attributes
+        // 1.   Value ---> Name of movie / director
+        // 2.   Link  ---> Place to redirect
+        var search = [
+            { value: "Taru" },
+            { value: "Vipul" },
+            { value: "Nitin" },
+            { value: "Maulik" }
+        ];
+        $('#nav__search').autocomplete({
+            // Using a custom callback
+            // simple search only compares string
+            // that since being done on server side
+            // we can just append all the results
+            source: function (request, response) {
+                response(search);
+            },
+            select: function (event, ui) {
+                $('#nav__search').val(ui.item.value)
+                return false;
+            }
+        }).data('ui-autocomplete')._renderItem = function (ul, item) {
+            return $('<li>')
+                .append('<a href="/search">' + item.value + '</a>')
+                .appendTo(ul);
+        };
+    });
+
 });
