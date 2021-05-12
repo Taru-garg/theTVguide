34a25dab76e273b50f7f3d40fb1a6facb99a32ed
nitingrover425 <nitin.grover425@gmail.com>
2021-05-12 20:01:06 +0530

    'insertQueryScript' Modified

--- utility/Database/insertQueryScript.py
+++ utility/Database/insertQueryScript.py
@@ -16,57 +16,54 @@ def createInsertQueryGenres(filename):
     # Make Query and insert it into a .sql file
     for genre in uniqueGenres:
         InsertQuery = statement + f"({genId}, '{genre}');\n"
-        with open('utility/Database/genreTable.sql', 'a+') as file:
+        with open('genreTable.sql', 'a+') as file:
             file.write(InsertQuery)
         genId = genId + 1
 
-
-#Use for movie_collection_data.csv file
-def CreateInsertQuery(filename):
+def createInsertQueryMovies(filename):
     Movie_data = pd.read_csv(filename)
-
-    print("Input data has %s rows" %Movie_data.shape[0] + " and %s columns" %Movie_data.shape[1])
-
-    #Table : MOVIES 
+    #print("[INFO] Input data has %s rows" %Movie_data.shape[0] + " and %s columns" %Movie_data.shape[1])
+    Movie_data = Movie_data.dropna(axis=0,how="all") #drop null records
     attributesM = ["ID","Movie_name","Overview","Release Date"] #required attributes [for now]
     statement = "INSERT INTO MOVIES VALUES "
-    for i in range(5):   #replace 5 with Movie_data.shape[0] (number of records)
-        statement += '('
+    for i in range(Movie_data.shape[0]):
+        values = '(' 
         for j in attributesM:
             if pd.isnull(Movie_data[j][i]):
-                s = "NULL"
+                values += "NULL,"
             else:
-                s = "'" + "%s" %Movie_data[j][i] + "'"
-            statement += s + ','
-        statement = statement.rstrip(',')
-        statement += '),'
-    statement = statement.rstrip(',')
-    statement += ';'
-    print('\n')
-    print(statement)
+                values += f"'{Movie_data[j][i]}',"
+        values = values.rstrip(',')
+        values += ");\n"
+        query = statement + values
+        with open('moviesTable.sql', 'a+') as file:
+            file.write(query)
 
-    #Table : movieImageData
+def createInsertQueryImages(filename):
+    image_data = pd.read_csv(filename)
+    image_data = image_data.dropna(axis=0,how="all") #drop null records
     attributesMI = ["ID","backdrop_path","poster_path"]  #required attributes 
     statement = "INSERT INTO movieImageData VALUES "
-    for i in range(5):   #replace 5 with Movie_data.shape[0] (number of records)
-        statement += '('
+    for i in range(image_data.shape[0]):
+        values = '(' 
         for j in attributesMI:
-            if pd.isnull(Movie_data[j][i]):
-                s = "NULL"
+            if pd.isnull(image_data[j][i]):
+                values += "NULL,"
             else:
-                s = "'" + "%s" %Movie_data[j][i] + "'"
-            statement += s + ','
-        statement = statement.rstrip(',')
-        statement += '),'
-    statement = statement.rstrip(',')
-    statement += ';'
-    print('\n')
-    print(statement)
+                values += f"'{image_data[j][i]}',"
+        values = values.rstrip(',')
+        values += ");\n"
+        query = statement + values
+        with open('movieImageTable.sql', 'a+') as file:
+            file.write(query)
+            
 
 
 if __name__=="__main__":
     filename = input("Enter file path:")
     createInsertQueryGenres(filename)
+    createInsertQueryMovies(filename)
+    createInsertQueryImages(filename)
 
 
 
