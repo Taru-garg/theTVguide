9056c522584f5df095d013e301ca57c930a6b332
Taru-garg <tarugarg2@gmail.com>
2021-05-16 18:30:49 +0530

    Connected HomePage to DB and Flask

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask.templating import render_template_string
 from utility import searchCleaning
 from utility.Database.python import conn
 from time import sleep
+
 # --------------------------------------------------------------------------------------------------------------------------------- #
 IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original"
 app = Flask(__name__)
 
 # --------------------------------------------------------------------------------------------------------------------------------- #
 
+
 @app.errorhandler(500)
 def error():
-    return render_template('error_page.html')
+    return render_template("error_page.html")
+
 
 @app.route("/movie/<id>")
 def movie(id):
@@ -37,7 +40,8 @@ def movie(id):
             date=results["initial_release_date"],
         )
     except:
-        return render_template('error_page.html')
+        return render_template("error_page.html")
+
 
 # --------------------------------------------------------------------------------------------------------------------------------- #
 
@@ -69,7 +73,40 @@ def searchEnter():
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # Queries
+    query1 = "EXECUTE Data @movId=10502"
+    query2 = "EXECUTE Data @movId=10509"
+    query3 = "EXECUTE Data @movId=10599"
+    # Getting the results
+    cursor1 = conn.exectueQuery(query1)
+    cursor2 = conn.exectueQuery(query2)
+    cursor3 = conn.exectueQuery(query3)
+    # Extracting Columns
+    columns = [column[0] for column in cursor1.description]
+    # Mapping columns to dict
+    results1 = dict(zip(columns, cursor1.fetchone()))
+    results2 = dict(zip(columns, cursor2.fetchone()))
+    results3 = dict(zip(columns, cursor3.fetchone()))
+
+    return render_template(
+        "index.html",
+        movie1=[
+            "/movie/" + str(results1["mov_id"]),
+            results1["title"],
+            IMAGE_BASE_URL + results1["backdrop"],
+            IMAGE_BASE_URL + results1["poster"],
+        ],
+        movie2=[            
+            "/movie/" + str(results2["mov_id"]),
+            results2["title"],
+            IMAGE_BASE_URL + results2["backdrop"],
+            IMAGE_BASE_URL + results2["poster"],],
+        movie3=[            
+            "/movie/" + str(results3["mov_id"]),
+            results3["title"],
+            IMAGE_BASE_URL + results3["backdrop"],
+            IMAGE_BASE_URL + results3["poster"],],
+    )
 
 
 # --------------------------------------------------------------------------------------------------------------------------------- #
