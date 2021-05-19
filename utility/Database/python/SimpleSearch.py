10ff586863d2b0fa0ce9f83db5672b4dca887c48
Taru-garg <tarugarg2@gmail.com>
2021-05-20 01:37:51 +0530

    Changes to app

--- utility/Database/python/SimpleSearch.py
+++ utility/Database/python/SimpleSearch.py
@@ -2,6 +2,6 @@ import utility.Database.python.conn as con
 
 
 def SimpleSearch(param, conn):
-    queryActor = "Select mov_id, title from movies where title like lower(?)"
+    queryActor = "Select mov_id, title from movies where lower(title) like lower(?)"
     result = con.processQueryMultiple(con.executeQuery(queryActor, param, conn))
     return result
